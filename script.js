7e9be1cd442316305d14466d1da5aed921422719
Shreyas2007 <84057370+shreyas391@users.noreply.github.com>
2023-05-14 22:45:32 +0530

    Update script.js
    Added event listener

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 AOS.init();
+
+var gmail = document.getElementById("gmail");
+
+gmail.addEventListener("click", function() {
+    document.getElementById("address").innerHTML = "shreyashema2007@gmail.com"
+});
